0d17824d93c0885c8d8c5f2b8e28d14ee3698c5d
Wilson Neto <owilsonneto@hotmail.com>
2024-05-02 16:23:45 -0300

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,8 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
+import Image from "next/image";
 
 
 export default function RootLayout({
@@ -7,10 +10,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Wilson', lastName: 'Neto' }
+
   return (
-    <main>
-        Sidebar
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image 
+              src="/icons/logo.svg"
+              width={30}
+              height={30}
+              alt="logo"
+            />
+            <div>
+              <MobileNav user={loggedIn} />
+            </div>
+
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
